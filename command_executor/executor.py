7338ffbb60bba996c3e3685001fbd1239345769f
silenc3502 <gcccompil3r@gmail.com>
2023-09-07 02:45:52 +0900

    add: 스레드간 정보 공유를 위한 Queue 정보 추가 [MSC-13]

--- command_executor/executor.py
+++ command_executor/executor.py
@@ -3,15 +3,15 @@ import threading
 import time
 
 
-
-
 class CommandExecutor:
-    def __init__(self):
+    def __init__(self, command_queue, transmit_queue):
         print("Command Executor Constructor")
         self.command_executor_thread = threading.Thread(target=self.executor, name='ExecutorThread')
         self.thread_lwp = None
         self.pid = None
         self.running = True
+        self.command_executor_queue = command_queue
+        self.transmit_response_queue = transmit_queue
 
     def executor(self):
         self.thread_lwp = threading.current_thread().native_id
