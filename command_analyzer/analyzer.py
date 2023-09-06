ec7dc2d1417f39b900e0624186743664f8b3dedb
silenc3502 <gcccompil3r@gmail.com>
2023-09-07 02:46:50 +0900

    add: 커맨드 분석을 위한 queue 공유 [MSC-12]

--- command_analyzer/analyzer.py
+++ command_analyzer/analyzer.py
@@ -4,12 +4,14 @@ import time
 
 
 class CommandAnalyzer:
-    def __init__(self):
+    def __init__(self, receive_queue, command_queue):
         print("Command Analyzer Constructor")
         self.command_analyzer_thread = threading.Thread(target=self.analyze, name='AnalyzerThread')
         self.thread_lwp = None
         self.pid = None
         self.running = True
+        self.receiver_command_data_queue = receive_queue
+        self.command_analyzer_queue = command_queue
 
     def analyze(self):
         self.thread_lwp = threading.current_thread().native_id
