9b69e99caa23a1a2097084f7769fcd515a0cc350
silenc3502 <gcccompil3r@gmail.com>
2023-09-07 19:44:24 +0900

    add: Analyzer Thread can read receive queue [MSC-12]

--- command_analyzer/analyzer.py
+++ command_analyzer/analyzer.py
@@ -6,22 +6,24 @@ import time
 class CommandAnalyzer:
     def __init__(self, receive_queue, command_queue):
         print("Command Analyzer Constructor")
-        self.command_analyzer_thread = threading.Thread(target=self.analyze, name='AnalyzerThread')
+        self.command_analyzer_thread = threading.Thread(target=self.analyze_command, name='AnalyzerThread')
         self.thread_lwp = None
         self.pid = None
         self.running = True
         self.receiver_command_data_queue = receive_queue
         self.command_analyzer_queue = command_queue
 
-    def analyze(self):
-        self.thread_lwp = threading.current_thread().native_id
-        print(f"LWP: {self.thread_lwp}")
-        self.pid = os.getpid()
-        print(f"Process PID: {self.pid}")
-        time.sleep(10)
-
     def start_analyzer_thread(self):
-        pass
+        print("start_analyzer_thread()")
+        self.command_analyzer_thread.start()
+
+    def analyze_command(self):
+        print("analyzer thread!")
+
+        while True:
+            received_data = self.receiver_command_data_queue.get()
+            print("received_data: ", received_data)
+            time.sleep(1)
 
     def stop_analyzer_thread(self):
         pass
